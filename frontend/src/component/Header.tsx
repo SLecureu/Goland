c3e4260d9553c18cbc9af4dfc854816e5c0415ea
Simon Lecureux <lecureuxsimon@orange.fr>
2024-07-01 13:49:55 +0200

    feat(header): added

---
+++ frontend/src/component/Header.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import "./Header.css"
+  
+export type User = {
+    id: number;
+    email: string;
+    name: string;
+    gender: 'male' | 'female' | 'other';
+    age: number;
+    firstName: string;
+    lastName: string;
+    created: Date;
+}
+
+function Header() {
+    const [user, setuser] = useState<User | null>(null);
+    
+    useEffect(() => {
+        fetch(`/api/auth`, {
+            method: `GET`,
+            headers: {
+                "Content-Type": `application/json`
+            }
+        })
+        .then((resp) => {
+            if (!resp.ok) return null;
+            return resp.json()
+        })
+        .then((data) => setuser(data))
+        .catch(err => console.log(err))
+    }, []);
+
+    return (
+        <header>
+            <h1>GOLAND</h1>
+            <div>
+                {user? user.firstName
+                : `login`}
+            </div>
+        </header>
+    )
+}
+
+export {Header}
