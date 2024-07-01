9c93dbb53d4f4ea92bb8fcb872bccc8140ab336b
clawz <cramananjaonapro@gmail.com>
2024-07-01 17:49:26 +0200

    feat! : added context

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -1,45 +1,18 @@
-import { useEffect, useState } from "react";
-import "./Header.css"
-  
-export type User = {
-    id: number;
-    email: string;
-    name: string;
-    gender: 'male' | 'female' | 'other';
-    age: number;
-    firstName: string;
-    lastName: string;
-    created: Date;
-}
+import { useContext } from "react";
+
+import { UserContext } from "./Context.tsx";
+
+import "./Header.css";
 
 function Header() {
-    const [user, setuser] = useState<User | null>(null);
-    
-    useEffect(() => {
-        fetch(`/api/auth`, {
-            method: `GET`,
-            headers: {
-                "Content-Type": `application/json`
-            },
-            credentials: "include"
-        })
-        .then((resp) => {
-            if (!resp.ok) return null;
-            return resp.json()
-        })
-        .then((data) => setuser(data))
-        .catch(err => console.log(err))
-    }, []);
+  const { user } = useContext(UserContext);
 
-    return (
-        <header>
-            <h1>GOLAND</h1>
-            <div>
-                {user? user.firstName
-                : `login`}
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <h1>GOLAND</h1>
+      <div>{user ? user.firstName : `login`}</div>
+    </header>
+  );
 }
 
-export {Header}
+export { Header };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
