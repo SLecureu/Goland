40e31a33ff17d918894ccbfc14e452b43a40e527
cramanan <cyrilramananjaona@gmail.com>
2024-07-04 17:29:16 +0200

    #

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,25 @@
 import "./Home.scss";
+import { Icons } from "../Imports";
 
 export default function Home() {
-    const block1 = () => <>Block 1</>;
-    const block2 = () => <>Block 2</>;
-    const block3 = () => <>Block 3</>;
-
     return (
         <main className="home-container">
-            {[block1, block2, block3].map((b, i) => (
-                <div key={i} className={`block-${i}`}>
-                    <div className="inside-block">{b()}</div>
+            <div className="block-1">
+                <div className="inside-block">
+                    <img
+                        src={Icons.home}
+                        alt="Home Icon"
+                        className="icons"
+                        width="50px"
+                    />
                 </div>
-            ))}
+            </div>
+            <div className="block-1">
+                <div className="inside-block"></div>
+            </div>
+            <div className="block-1">
+                <div className="inside-block"></div>
+            </div>
         </main>
     );
 }
