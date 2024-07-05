301a8122117cbf88e6a348c8ab093efd87ba9644
Simon Lecureux <snoprolecureux@gmail.com>
2024-07-05 14:53:20 +0200

    feat(footer): linkedin link

--- src/Imports.ts
+++ src/Imports.ts
@@ -11,11 +11,13 @@ import Overview from "./pages/Overview";
 import home from "./assets/icons/home.svg";
 import post from "./assets/icons/post.svg";
 import github from "./assets/icons/github.svg";
+import linkedin from "./assets/icons/linkedin.svg";
 
 const Icons = {
   home,
   github,
   post,
+  linkedin,
 };
 
 export { Header, Login, Register, Home, Post, Footer, User, Overview, Icons };
