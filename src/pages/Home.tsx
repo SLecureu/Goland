249002972fbc5da3565d6209f9f8c9f038ceec30
Simon Lecureux <snoprolecureux@gmail.com>
2024-07-08 10:46:12 +0200

    fix(homepage): link to pseudo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,69 +5,69 @@ import { Icons, Post } from "../Imports";
 import { useEffect, useState } from "react";
 
 type Post = {
-    id: string;
-    userID: string;
-    username: string;
-    categories: string[];
-    content: string;
-    created: string;
+  id: string;
+  userID: string;
+  username: string;
+  categories: string[];
+  content: string;
+  created: string;
 };
 
 export default function Home() {
-    const [posts, setPosts] = useState<Post[]>([]);
-    // for (let index = 0; index < 10; index++) {
-    //     posts.push({
-    //         id: "Foo",
-    //         userID: "Bar",
-    //         username: "John Doe",
-    //         categories: [],
-    //         content: "Baz",
-    //         created: new Date().toString(),
-    //     });
-    // }
+  const [posts, setPosts] = useState<Post[]>([]);
+  // for (let index = 0; index < 10; index++) {
+  //     posts.push({
+  //         id: "Foo",
+  //         userID: "Bar",
+  //         username: "John Doe",
+  //         categories: [],
+  //         content: "Baz",
+  //         created: new Date().toString(),
+  //     });
+  // }
 
-    useEffect(() => {
-        fetch("/api/posts")
-            .then((resp) => {
-                if (resp.ok) return resp.json();
-                return [];
-            })
-            .then(setPosts);
-    }, []);
+  useEffect(() => {
+    fetch("/api/posts")
+      .then((resp) => {
+        if (resp.ok) return resp.json();
+        return [];
+      })
+      .then(setPosts);
+  }, []);
 
-    return (
-        <main className="home-container">
-            <div className="block-0">
-                <div className="inside-block">
-                    <img
-                        src={Icons.home}
-                        alt="Home Icon"
-                        className="icons"
-                        width="50px"
-                    />
-                    <span>All posts</span>
-                </div>
-                <Link to="/post" className="inside-block">
-                    <img
-                        src={Icons.post}
-                        alt="Home Icon"
-                        className="icons"
-                        width="50px"
-                    />
-                    <span>Create a Post</span>
-                </Link>
-            </div>
-            <div className="block-1 posts">
-                {posts.map((post, index) => (
-                    <div key={index} className="post">
-                        <Link to={`/user/${post.userID}`}>
-                            <h2>{post.username}</h2>
-                        </Link>
-                        <p>{post.content}</p>
-                    </div>
-                ))}
-            </div>
-            <div className="block-2 ">Foo</div>
-        </main>
-    );
+  return (
+    <main className="home-container">
+      <div className="block-0">
+        <div className="inside-block">
+          <img
+            src={Icons.home}
+            alt="Home Icon"
+            className="icons"
+            width="50px"
+          />
+          <span>All posts</span>
+        </div>
+        <Link to="/post" className="inside-block">
+          <img
+            src={Icons.post}
+            alt="Home Icon"
+            className="icons"
+            width="50px"
+          />
+          <span>Create a Post</span>
+        </Link>
+      </div>
+      <div className="block-1 posts">
+        {posts.map((post, index) => (
+          <div key={index} className="post">
+            <h2>
+              <Link to={`/user/${post.userID}`}>{post.username}</Link>
+            </h2>
+            <p>{post.content}</p>
+          </div>
+        ))}
+      </div>
+      <div className="block-2 ">Foo</div>
+    </main>
+  );
 }
