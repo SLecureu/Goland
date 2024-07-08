1092b251de3b6acd67cd30f93e2691015e6c5001
cramanan <cyrilramananjaona@gmail.com>
2024-07-08 15:34:34 +0200

    refactor: fixed bad code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,62 +8,52 @@ import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 
 export default function Home() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  // for (let index = 0; index < 10; index++) {
-  //     posts.push({
-  //         id: "Foo",
-  //         userID: "Bar",
-  //         username: "John Doe",
-  //         categories: [],
-  //         content: "Baz".repeat(100),
-  //         created: new Date().toString(),
-  //     });
-  // }
+    const [posts, setPosts] = useState<Post[]>([]);
 
-  useEffect(() => {
-    fetch("/api/posts")
-      .then((resp) => {
-        if (resp.ok) return resp.json();
-        return [];
-      })
-      .then(setPosts);
-  }, []);
+    useEffect(() => {
+        fetch("/api/posts")
+            .then((resp) => {
+                if (resp.ok) return resp.json();
+                return [];
+            })
+            .then(setPosts);
+    }, []);
 
-  return (
-    <Layout>
-      <main className="home-container">
-        <div className="block-0">
-          <div className="inside-block">
-            <img
-              src={Icons.home}
-              alt="Home Icon"
-              className="icons"
-              width="50px"
-            />
-            <span>All posts</span>
-          </div>
-          <Link to="/post" className="inside-block">
-            <img
-              src={Icons.post}
-              alt="Home Icon"
-              className="icons"
-              width="50px"
-            />
-            <span>Create a Post</span>
-          </Link>
-        </div>
-        <div className="block-1 posts">
-          {posts.map((post, index) => (
-            <div key={index} className="post">
-              <Link to={`/user/${post.userID}`}>
-                <h2>{post.username}</h2>
-              </Link>
-              <p>{post.content}</p>
-            </div>
-          ))}
-        </div>
-        <div className="block-2 ">Foo</div>
-      </main>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <main className="home-container">
+                <div className="block-0">
+                    <div className="inside-block">
+                        <img
+                            src={Icons.home}
+                            alt="Home Icon"
+                            className="icons"
+                            width="50px"
+                        />
+                        <span>All posts</span>
+                    </div>
+                    <Link to="/post" className="inside-block">
+                        <img
+                            src={Icons.post}
+                            alt="Home Icon"
+                            className="icons"
+                            width="50px"
+                        />
+                        <span>Create a Post</span>
+                    </Link>
+                </div>
+                <div className="block-1 posts">
+                    {posts.map((post, index) => (
+                        <div key={index} className="post">
+                            <Link to={`/user/${post.userID}`}>
+                                <h2>{post.username}</h2>
+                            </Link>
+                            <p>{post.content}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="block-2 ">Foo</div>
+            </main>
+        </Layout>
+    );
 }
--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -7,66 +7,66 @@ import { Protected } from "../Imports";
 import { Layout } from "../Imports";
 
 type Inputs = {
-  content: string;
-  categories: string[];
+    content: string;
+    categories: string[];
 };
 
 export function PublishPost() {
-  const navigate = useNavigate();
-  const { register, handleSubmit } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    if (!data.content) return;
-    data.categories = data.content.match(/#([a-zA-Z\d-]+)/g) || [];
-    await fetch("/api/post", {
-      credentials: "include",
-      method: "POST",
-      body: JSON.stringify(data),
-    }).then((resp) => {
-      if (!resp.ok) return;
-      navigate("/");
-    });
-  };
+    const navigate = useNavigate();
+    const { register, handleSubmit } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        if (!data.content) return;
+        data.categories = data.content.match(/#([a-zA-Z\d-]+)/g) || [];
+        await fetch("/api/post", {
+            credentials: "include",
+            method: "POST",
+            body: JSON.stringify(data),
+        }).then((resp) => {
+            if (!resp.ok) return;
+            navigate("/");
+        });
+    };
 
-  return (
-    <Protected>
-      <Layout>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="content">Post Content</label>
-          <textarea id="content" {...register("content")} />
-          <button type="submit">Post</button>
-        </form>
-      </Layout>
-    </Protected>
-  );
+    return (
+        <Protected>
+            <Layout>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor="content">Post Content</label>
+                    <textarea id="content" {...register("content")} />
+                    <button type="submit">Post</button>
+                </form>
+            </Layout>
+        </Protected>
+    );
 }
 
 export function GetPost() {
-  const { id } = useParams();
-  const [publication, setPublication] = useState<Post | null>(null);
+    const { id } = useParams();
+    const [publication, setPublication] = useState<Post | null>(null);
 
-  useEffect(() => {
-    fetch(`/api/post/${id}`, {
-      method: `GET`,
-      headers: {
-        "Content-Type": `application/json`,
-      },
-      credentials: "include",
-    })
-      .then((resp) => {
-        if (!resp.ok) {
-          throw new Error(`Failed to fetch post: ${resp.status}`);
-        }
-        return resp.json();
-      })
-      .then(setPublication)
-      .catch(console.log);
-  }, []);
+    useEffect(() => {
+        fetch(`/api/post/${id}`, {
+            method: `GET`,
+            headers: {
+                "Content-Type": `application/json`,
+            },
+            credentials: "include",
+        })
+            .then((resp) => {
+                if (!resp.ok) {
+                    throw new Error(`Failed to fetch post: ${resp.status}`);
+                }
+                return resp.json();
+            })
+            .then(setPublication)
+            .catch(console.log);
+    }, []);
 
-  return publication ? (
-    <main>
-      <h2>{publication?.content}</h2>
-    </main>
-  ) : (
-    <Navigate to="/*" />
-  );
+    if (!publication) return <Navigate to="/*" />;
+
+    return (
+        <main>
+            <h2>{publication?.content}</h2>
+        </main>
+    );
 }
