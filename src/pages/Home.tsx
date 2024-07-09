e86d3e7c328aaf19a39b5c091f895586774109f1
cramanan <cyrilramananjaona@gmail.com>
2024-07-09 11:10:57 +0200

    fix: fixed user bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,58 +8,58 @@ import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 
 export default function Home() {
-  const [posts, setPosts] = useState<Post[]>([]);
+    const [posts, setPosts] = useState<Post[]>([]);
 
-  useEffect(() => {
-    fetch("/api/posts")
-      .then((resp) => {
-        if (resp.ok) return resp.json();
-        return [];
-      })
-      .then(setPosts);
-  }, []);
+    useEffect(() => {
+        fetch("/api/posts")
+            .then((resp) => {
+                if (resp.ok) return resp.json();
+                return [];
+            })
+            .then(setPosts);
+    }, []);
 
-  return (
-    <Layout>
-      <main className="home-container">
-        <div className="block-0">
-          <div className="inside-block">
-            <img
-              src={Icons.home}
-              alt="Home Icon"
-              className="icons"
-              width="50px"
-            />
-            <span>All posts</span>
-          </div>
-          <Link to="/post" className="inside-block">
-            <img
-              src={Icons.post}
-              alt="Home Icon"
-              className="icons"
-              width="50px"
-            />
-            <span>Create a Post</span>
-          </Link>
-        </div>
-        <div className="block-1 posts">
-          {posts.map((post, index) => (
-            <div key={index} className="post">
-              <h2>
-                <Link to={`/user/${post.userID}`}>
-                  <span>{post.username}</span>{" "}
-                </Link>
-              </h2>
-              <p>
-                <Link to={`/post/${post.id}`}>
-                  <span>{post.content}</span>
-                </Link>
-              </p>
-            </div>
-          ))}
-        </div>
-        <div className="block-2 ">Foo</div>
-      </main>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <main className="home-container">
+                <div className="block-0">
+                    <div className="inside-block">
+                        <img
+                            src={Icons.home}
+                            alt="Home Icon"
+                            className="icons"
+                            width="50px"
+                        />
+                        <span>All posts</span>
+                    </div>
+                    <Link to="/post" className="inside-block">
+                        <img
+                            src={Icons.post}
+                            alt="Home Icon"
+                            className="icons"
+                            width="50px"
+                        />
+                        <span>Create a Post</span>
+                    </Link>
+                </div>
+                <div className="block-1 posts">
+                    {posts.map((post, index) => (
+                        <div key={index} className="post">
+                            <h2>
+                                <Link to={`/user/${post.userID}`}>
+                                    <span>{post.username}</span>{" "}
+                                </Link>
+                            </h2>
+                            <p>
+                                <Link to={`/post/${post.id}`}>
+                                    <span>{post.content}</span>
+                                </Link>
+                            </p>
+                        </div>
+                    ))}
+                </div>
+                <div className="block-2 ">Foo</div>
+            </main>
+        </Layout>
+    );
 }
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,38 +1,46 @@
 import { useEffect, useState } from "react";
-import { useParams, Navigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { User } from "../components/Context";
 import { Layout } from "../Imports";
 
 function UserPage() {
-  const { id } = useParams();
-  const [user, setUser] = useState<User | null>(null);
+    const { id } = useParams();
+    const [user, setUser] = useState<User | null>(null);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    fetch(`/api/user/${id}`, {
-      method: `GET`,
-      headers: {
-        "Content-Type": `application/json`,
-      },
-      credentials: "include",
-    })
-      .then((resp) => {
-        if (!resp.ok) {
-          throw new Error(`Failed to fetch user: ${resp.status}`);
-        }
-        return resp.json();
-      })
-      .then(setUser)
-      .catch(console.log);
-  }, []);
-  return user ? (
-    <Layout>
-      <main>
-        <h2>{user.name}</h2>
-      </main>
-    </Layout>
-  ) : (
-    <Navigate to="/*" />
-  );
+    useEffect(() => {
+        fetch(`/api/user/${id}`, {
+            method: `GET`,
+            headers: {
+                "Content-Type": `application/json`,
+            },
+            credentials: "include",
+        })
+            .then((resp) => {
+                if (!resp.ok) {
+                    navigate("/*");
+                    return null;
+                }
+                return resp.json();
+            })
+            .then(setUser)
+            .catch(console.log);
+    }, []);
+
+    if (!user)
+        return (
+            <Layout>
+                <div>Loading</div>
+            </Layout>
+        );
+
+    return (
+        <Layout>
+            <main>
+                <h2>{user.name}</h2>
+            </main>
+        </Layout>
+    );
 }
 
 export default UserPage;
