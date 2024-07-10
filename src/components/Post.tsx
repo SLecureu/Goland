cb74784b21b83e18b6952ce97f41f842d70832fc
Simon Lecureux <snoprolecureux@gmail.com>
2024-07-10 11:01:04 +0200

    fix(postpage): link to postpage

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 import { PostType } from "./Context";
 
 export default function Post({ post, key }: { post: PostType; key: number }) {
-    return (
-        <div className="post" key={key}>
-            <Link to={`/user/${post.userID}`}>
-                <h2>{post.username}</h2>
-            </Link>
-            <p>{post.content}</p>
-        </div>
-    );
+  return (
+    <div className="post" key={key}>
+      <h2>
+        <Link to={`/user/${post.userID}`}>{post.username}</Link>
+      </h2>
+      <p>
+        <Link to={`/post/${post.id}`}>{post.content}</Link>
+      </p>
+    </div>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,41 +9,41 @@ import Post from "../components/Post";
 import Loader from "../components/Loader";
 
 export default function Home() {
-    const [posts, setPosts] = useState<PostType[]>([]);
+  const [posts, setPosts] = useState<PostType[]>([]);
 
-    useEffect(() => {
-        fetch("/api/posts")
-            .then((resp) => {
-                if (resp.ok) return resp.json();
-                return [];
-            })
-            .then(setPosts);
-    }, []);
+  useEffect(() => {
+    fetch("/api/posts")
+      .then((resp) => {
+        if (resp.ok) return resp.json();
+        return [];
+      })
+      .then(setPosts);
+  }, []);
 
-    return (
-        <main className="home-container">
-            <div className="block-0">
-                <Link to="/post" className="inside-block">
-                    <img
-                        src={Icons.post}
-                        alt="Home Icon"
-                        className="icons"
-                        width="50px"
-                    />
-                    <span>Create a Post</span>
-                </Link>
-            </div>
-            <div className="block-1 posts">
-                {posts.length === 0 ? (
-                    <Loader />
-                ) : (
-                    posts.map((post, index) => {
-                        setTimeout(() => {}, 1000);
-                        return <Post post={post} key={index} />;
-                    })
-                )}
-            </div>
-            <div className="block-2 ">Foo</div>
-        </main>
-    );
+  return (
+    <main className="home-container">
+      <div className="block-0">
+        <Link to="/post" className="inside-block">
+          <img
+            src={Icons.post}
+            alt="Home Icon"
+            className="icons"
+            width="50px"
+          />
+          <span>Create a Post</span>
+        </Link>
+      </div>
+      <div className="block-1 posts">
+        {posts.length === 0 ? (
+          <Loader />
+        ) : (
+          posts.map((post, index) => {
+            setTimeout(() => {}, 1000);
+            return <Post post={post} key={index} />;
+          })
+        )}
+      </div>
+      <div className="block-2 ">Foo</div>
+    </main>
+  );
 }
