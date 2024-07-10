140c9e9d3ead85053798eb9fad2ffdfd7ba5dad2
Simon Lecureux <snoprolecureux@gmail.com>
2024-07-10 14:23:58 +0200

    feat(postpage): navigation

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,21 +1,21 @@
 import "./Error.scss";
 
 const ErrorPage = ({ code }: { code: number }) => {
-    let msg = "";
-    switch (code) {
-        case 404:
-            msg = "Not Found";
-            break;
+  let msg = "";
+  switch (code) {
+    case 404:
+      msg = "Not Found";
+      break;
 
-        default:
-            break;
-    }
-    return (
-        <div className="error-container">
-            <h2>{code}</h2>
-            <p>{msg}</p>
-        </div>
-    );
+    default:
+      break;
+  }
+  return (
+    <div className="error-container">
+      <h2>{code}</h2>
+      <p>{msg}</p>
+    </div>
+  );
 };
 
 export default ErrorPage;
--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -9,64 +9,100 @@ import { Protected } from "../Imports";
 import "./PostPage.scss";
 
 type Inputs = {
-    content: string;
-    categories: string[];
+  content: string;
+  categories: string[];
 };
 
 const HashTagRexExp = /(?<=#)[\w\d\.]+/g;
 
 export function PostPage() {
-    const navigate = useNavigate();
-    const { register, handleSubmit } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = async (data) => {
-        if (!data.content) return;
-        data.categories = data.content.match(HashTagRexExp) || [];
-        await fetch("/api/post", {
-            credentials: "include",
-            method: "POST",
-            body: JSON.stringify(data),
-        }).then((resp) => {
-            if (!resp.ok) return;
-            navigate("/");
-        });
-    };
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    if (!data.content) return;
+    data.categories = data.content.match(HashTagRexExp) || [];
+    await fetch("/api/post", {
+      credentials: "include",
+      method: "POST",
+      body: JSON.stringify(data),
+    }).then((resp) => {
+      if (!resp.ok) return;
+      navigate("/");
+    });
+  };
 
-    return (
-        <Protected>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="content">Post Content</label>
-                <textarea id="content" {...register("content")} />
-                <button type="submit">Post</button>
-            </form>
-        </Protected>
-    );
+  return (
+    <Protected>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="content">Post Content</label>
+        <textarea id="content" {...register("content")} />
+        <button type="submit">Post</button>
+      </form>
+    </Protected>
+  );
 }
 
 export function GetPost() {
-    const { id } = useParams();
-    const [post, setPost] = useState<PostType | null>(null);
+  const { id } = useParams();
+  const [post, setPost] = useState<PostType | null>(null);
+  const [classes, setClasses] = useState({
+    c1: true,
+    c2: false,
+    c3: false,
+  });
 
-    useEffect(() => {
-        fetch(`/api/post/${id}`, {
-            method: `GET`,
-            headers: {
-                "Content-Type": `application/json`,
-            },
-            credentials: "include",
-        })
-            .then((resp) => {
-                if (!resp.ok) return null;
-                return resp.json();
-            })
-            .then(setPost)
-            .catch(console.log);
-    }, [id]);
+  useEffect(() => {
+    fetch(`/api/post/${id}`, {
+      method: `GET`,
+      headers: {
+        "Content-Type": `application/json`,
+      },
+      credentials: "include",
+    })
+      .then((resp) => {
+        if (!resp.ok) return null;
+        return resp.json();
+      })
+      .then(setPost)
+      .catch(console.log);
+  }, [id]);
 
-    if (!post) return <ErrorPage code={404} />;
+  if (!post) return <ErrorPage code={404} />;
 
+  const Factorize = ({ text, index }: { text: string; index: number }) => {
     return (
-        <main>
-            <h2>{post.content}</h2>
-        </main>
+      <li
+        onClick={() =>
+          index == 0
+            ? setClasses({ ...classes, c1: true, c2: false, c3: false })
+            : index == 1
+            ? setClasses({ ...classes, c1: false, c2: true, c3: false })
+            : setClasses({ ...classes, c1: false, c2: false, c3: true })
+        }
+      >
+        {text}
+        <div
+          className={
+            (classes as { [key: string]: boolean })[`c${index + 1}`]
+              ? "selected-li"
+              : ""
+          }
+        ></div>
+      </li>
     );
+  };
+
+  return (
+    <main className="postpage">
+      <div className="banner">
+        <nav>
+          <ul>
+            {["Post", "Comments", "Data"].map((t, i) => {
+              return <Factorize text={t} index={i} />;
+            })}
+          </ul>
+        </nav>
+      </div>
+    </main>
+  );
 }
