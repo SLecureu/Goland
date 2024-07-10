8be4c0e4244af810484aceccd3a4494263e9bfa0
Simon Lecureux <snoprolecureux@gmail.com>
2024-07-10 14:23:58 +0200

    feat(postpage): navigation

--- src/components/Context.ts
+++ src/components/Context.ts
@@ -1,33 +1,33 @@
 import { createContext } from "react";
 
 export type User = {
-    id: number;
-    email: string;
-    name: string;
-    gender: "male" | "female" | "other";
-    age: number;
-    firstName: string;
-    lastName: string;
-    created: Date;
+  id: number;
+  email: string;
+  name: string;
+  gender: "male" | "female" | "other";
+  age: number;
+  firstName: string;
+  lastName: string;
+  created: Date;
 };
 
 export type PostType = {
-    id: string;
-    userID: string;
-    username: string;
-    categories: string[];
-    content: string;
-    created: string;
+  id: string;
+  userID: string;
+  username: string;
+  categories: string[];
+  content: string;
+  created: string;
 };
 
 type InitialType = {
-    user: User | null;
-    setUser: (newUser: User | null) => void;
-    loading: boolean;
+  user: User | null;
+  setUser: (newUser: User | null) => void;
+  loading: boolean;
 };
 
 export const UserContext = createContext<InitialType>({
-    user: null,
-    setUser: console.log,
-    loading: true,
+  user: null,
+  setUser: console.log,
+  loading: true,
 });
