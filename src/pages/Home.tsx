889cd1b267fc707118d1315b133a0b598eeeae31
Simon Lecureux <snoprolecureux@gmail.com>
2024-07-11 10:10:00 +0200

    style(homepage): block-0 style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,44 +9,46 @@ import Post from "../components/Post";
 import Loader from "../components/Loader";
 
 export default function Home() {
-    const [posts, setPosts] = useState<PostType[]>([]);
+  const [posts, setPosts] = useState<PostType[]>([]);
 
-    useEffect(() => {
-        fetch("/api/posts")
-            .then((resp) => {
-                if (resp.ok) return resp.json();
-                return [];
+  useEffect(() => {
+    fetch("/api/posts")
+      .then((resp) => {
+        if (resp.ok) return resp.json();
+        return [];
+      })
+      .then(setPosts);
+  }, []);
+
+  return (
+    <main className="home-container">
+      <div className="block" id="block-0">
+        <div className="inside-block">
+          <Link to="/post" style={{ display: "flex", alignItems: "center" }}>
+            <img
+              src={Icons.post}
+              alt="Home Icon"
+              className="icons"
+              width="50px"
+            />
+            <span>Create a Post</span>
+          </Link>
+        </div>
+      </div>
+      <div className="block" id="block-1">
+        <div className="posts">
+          {posts.length === 0 ? (
+            <Loader width="100px" height="100px" />
+          ) : (
+            posts.map((post, index) => {
+              return <Post key={index} post={post} />;
             })
-            .then(setPosts);
-    }, []);
-
-    return (
-        <main className="home-container">
-            <div className="block" id="block-0">
-                <Link to="/post" className="inside-block">
-                    <img
-                        src={Icons.post}
-                        alt="Home Icon"
-                        className="icons"
-                        width="50px"
-                    />
-                    <span>Create a Post</span>
-                </Link>
-            </div>
-            <div className="block" id="block-1">
-                <div className="posts">
-                    {posts.length === 0 ? (
-                        <Loader width="100px" height="100px" />
-                    ) : (
-                        posts.map((post, index) => {
-                            return <Post key={index} post={post} />;
-                        })
-                    )}
-                </div>
-            </div>
-            <div className="block" id="block-2">
-                Foo
-            </div>
-        </main>
-    );
+          )}
+        </div>
+      </div>
+      <div className="block" id="block-2">
+        Foo
+      </div>
+    </main>
+  );
 }
