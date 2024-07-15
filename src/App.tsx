54324eed6752e701d49333a4de6195a024532def
Simon Lecureux <snoprolecureux@gmail.com>
2024-07-15 11:43:04 +0200

    fix(pages): style problem at merge

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@
 import { Route, Routes } from "react-router-dom";
 
 import {
-    Header,
-    Register,
-    Login,
-    Home,
-    PostPage,
-    User,
-    Overview,
-    GetPost,
-    Category,
-    ErrorPage,
-    Protected,
-    Footer,
+  Header,
+  Register,
+  Login,
+  Home,
+  PostPage,
+  User,
+  Overview,
+  GetPost,
+  Category,
+  ErrorPage,
+  Protected,
+  Footer,
 } from "./Imports.ts";
 
 // CSS
@@ -22,36 +22,34 @@ import "./App.scss";
 import UserContextProvider from "./components/ContextProvider.tsx";
 
 function App() {
-    return (
-        <UserContextProvider>
-            <Header />
-            <main>
-                <Routes>
-                    <Route path="/user/">
-                        <Route path=":id" element={<User />} />
-                        <Route path="" element={<ErrorPage code={404} />} />
-                    </Route>
-                    <Route path="/post/">
-                        <Route path=":id" element={<GetPost />} />
-                        <Route path="" element={<PostPage />} />
-                    </Route>
-                    <Route path="/category/">
-                        <Route path=":id" element={<Category />} />
-                        <Route path="" element={<ErrorPage code={404} />} />
-                    </Route>
-                    <Route
-                        path="overview"
-                        element={<Protected children={<Overview />} />}
-                    />
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="*" element={<ErrorPage code={404} />} />
-                </Routes>
-            </main>
-            <Footer />
-        </UserContextProvider>
-    );
+  return (
+    <UserContextProvider>
+      <Header />
+      <Routes>
+        <Route path="/user/">
+          <Route path=":id" element={<User />} />
+          <Route path="" element={<ErrorPage code={404} />} />
+        </Route>
+        <Route path="/post/">
+          <Route path=":id" element={<GetPost />} />
+          <Route path="" element={<PostPage />} />
+        </Route>
+        <Route path="/category/">
+          <Route path=":id" element={<Category />} />
+          <Route path="" element={<ErrorPage code={404} />} />
+        </Route>
+        <Route
+          path="overview"
+          element={<Protected children={<Overview />} />}
+        />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<ErrorPage code={404} />} />
+      </Routes>
+      <Footer />
+    </UserContextProvider>
+  );
 }
 
 export default App;
