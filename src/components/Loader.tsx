7a889b062ed24cdc503cf8574bd3154c46431bd9
Simon Lecureux <snoprolecureux@gmail.com>
2024-07-15 13:35:44 +0200

    fix(loader): <main>

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,14 +4,11 @@ import { Icons } from "../assets/assets";
 interface LoaderProps extends ImgHTMLAttributes<HTMLImageElement> {}
 
 const Loader: FC<LoaderProps> = ({ width, height }) => {
-    return (
-        <img
-            src={Icons.logo}
-            className="loader"
-            width={width}
-            height={height}
-        />
-    );
+  return (
+    <main>
+      <img src={Icons.logo} className="loader" width={width} height={height} />
+    </main>
+  );
 };
 
 export default Loader;
