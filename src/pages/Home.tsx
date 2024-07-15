8d9ba7f189540ac3dded9265e00774fa7301af65
Simon Lecureux <snoprolecureux@gmail.com>
2024-07-15 11:48:58 +0200

    docs(comment)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,57 +9,57 @@ import Post from "../components/Post";
 import Loader from "../components/Loader";
 
 export default function Home() {
-    const [posts, setPosts] = useState<PostType[]>([
-        {
-            id: "",
-            userID: "",
-            username: "shgLJHGLHFGMFgeiuGFIYG",
-            content: "hjgvoshgfishgp<ibvpisbvpisbsbljhbfhbvl<hbvlh<b",
-            created: "",
-            categories: [],
-        },
-    ]);
+  const [posts, setPosts] = useState<PostType[]>([
+    // {
+    //     id: "",
+    //     userID: "",
+    //     username: "shgLJHGLHFGMFgeiuGFIYG",
+    //     content: "hjgvoshgfishgp<ibvpisbvpisbsbljhbfhbvl<hbvlh<b",
+    //     created: "",
+    //     categories: [],
+    // },
+  ]);
 
-    useEffect(() => {
-        fetch("/api/posts")
-            .then((resp) => {
-                if (resp.ok) return resp.json();
-                return [];
+  useEffect(() => {
+    fetch("/api/posts")
+      .then((resp) => {
+        if (resp.ok) return resp.json();
+        return [];
+      })
+      .then(setPosts);
+  }, []);
+
+  return (
+    <main className="home-container">
+      <div className="block" id="block-0">
+        <div className="inside-block">
+          <Link to="/post">
+            <img
+              src={Icons.post}
+              alt="Post Icon"
+              className="icons"
+              width="50px"
+            />
+            <span>Create a Post</span>
+          </Link>
+        </div>
+      </div>
+      <div className="block" id="block-1">
+        <div className="posts">
+          {posts.length === 0 ? (
+            <Loader width="100px" height="100px" />
+          ) : (
+            posts.map((post, index) => {
+              return <Post key={index} post={post} />;
             })
-            .then(setPosts);
-    }, []);
-
-    return (
-        <main className="home-container">
-            <div className="block" id="block-0">
-                <div className="inside-block">
-                    <Link to="/post">
-                        <img
-                            src={Icons.post}
-                            alt="Post Icon"
-                            className="icons"
-                            width="50px"
-                        />
-                        <span>Create a Post</span>
-                    </Link>
-                </div>
-            </div>
-            <div className="block" id="block-1">
-                <div className="posts">
-                    {posts.length === 0 ? (
-                        <Loader width="100px" height="100px" />
-                    ) : (
-                        posts.map((post, index) => {
-                            return <Post key={index} post={post} />;
-                        })
-                    )}
-                </div>
-            </div>
-            <div className="block" id="block-2">
-                <div id="searchbars">
-                    <input type="text" />
-                </div>
-            </div>
-        </main>
-    );
+          )}
+        </div>
+      </div>
+      <div className="block" id="block-2">
+        <div id="searchbars">
+          <input type="text" />
+        </div>
+      </div>
+    </main>
+  );
 }
