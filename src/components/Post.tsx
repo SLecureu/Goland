6b97911e7e58b263d6aed1f006048a501543b5ad
Simon Lecureux <snoprolecureux@gmail.com>
2024-09-03 12:45:37 +0200

    #

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import { PostType } from "./Context";
+import "./Post.scss";
 
 export default function Post({ post }: { post: PostType }) {
     return (
-        <a className="post" href={`/post/${post.id}`}>
+        <a className="post_" href={`/post/${post.id}`}>
             <h2>
                 <Link to={`/user/${post.userID}`}>{post.username}</Link>
             </h2>
             {post.content && (
-                <p>
-                    <Link to={`/post/${post.id}`}>{post.content}</Link>
-                </p>
+                <Link to={`/post/${post.id}`}>
+                    <p>{post.content}</p>
+                </Link>
             )}
         </a>
     );
